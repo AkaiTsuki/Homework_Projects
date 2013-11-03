451535c56fcc04b588bc92f71bac98e9a306e50c
jiachiliu <jiachiliu@Jiachis-Computer.local>
2013-11-02 22:33:27 -0400

    modified demo code

--- DM/final project/demo.py
+++ DM/final project/demo.py
@@ -12,10 +12,19 @@ if __name__ == '__main__':
     rpath = 'yelp_training_set_review.json'
     cpath = 'yelp_training_set_checkin.json'
 
+    # Initialize the data center object
     dataCenter = DataCenter()
+    # load all json data to the data center
     dataCenter.loadAll(upath,bpath,rpath,cpath)
+    
+    # get data from data center
+    # the following 4 data are all dictionary
+    users = dataCenter.getUserData()
+    business = dataCenter.getBusinessData()
+    reviews = dataCenter.getReviewData()
+    checkIn = dataCenter.getCheckInData()
 
-    print len(dataCenter.getUserData())
-    print len(dataCenter.getBusinessData())
-    print len(dataCenter.getReviewData())
-    print len(dataCenter.getCheckInData())
+    print len(users)
+    print len(business)
+    print len(reviews)
+    print len(checkIn)
